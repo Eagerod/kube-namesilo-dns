00585c56b87489cd93229d0fa0b3e9d6623c8ff8
Aleem Haji <hajial@gmail.com>
2022-10-10 11:02:36 -0400

    Refactor rr equals method

--- pkg/namesilo_api/resource_record.go
+++ pkg/namesilo_api/resource_record.go
@@ -19,29 +19,28 @@ func (r ResourceRecord) Equals(other interface{}) bool {
 		return false
 	}
 
-	if r == other {
-		return true
-	}
-
-	var otherRR ResourceRecord
 	otherRRp, ok := other.(*ResourceRecord)
 	if ok {
-		otherRR = *(otherRRp)
-	} else {
-		otherRR, ok = other.(ResourceRecord)
-		if !ok {
-			return false
-		}
+		return r.EqualsRecord(*otherRRp)
 	}
 
+	otherRR, ok := other.(ResourceRecord)
+	if !ok {
+		return false
+	}
+
+	return r.EqualsRecord(otherRR)
+}
+
+func (r ResourceRecord) EqualsRecord(other ResourceRecord) bool {
 	matchIds := true
-	if r.RecordId != "" && otherRR.RecordId != "" {
-		matchIds = r.RecordId == otherRR.RecordId
+	if r.RecordId != "" && other.RecordId != "" {
+		matchIds = r.RecordId == other.RecordId
 	}
 	return matchIds &&
-		r.Type == otherRR.Type &&
-		r.Host == otherRR.Host &&
-		r.Value == otherRR.Value &&
-		r.TTL == otherRR.TTL &&
-		r.Distance == otherRR.Distance
+		r.Type == other.Type &&
+		r.Host == other.Host &&
+		r.Value == other.Value &&
+		r.TTL == other.TTL &&
+		r.Distance == other.Distance
 }
