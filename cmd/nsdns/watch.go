a3008e0ec8b8ada7b3daad47954687759fba2511
Aleem Haji <hajial@gmail.com>
2023-05-01 21:18:02 -0400

    Adds the ability to use leases to run the domain manager

--- cmd/nsdns/watch.go
+++ cmd/nsdns/watch.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"context"
+	"errors"
 	"os"
 	"os/signal"
 	"syscall"
@@ -11,9 +13,12 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	networkingv1 "k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/leaderelection"
+	"k8s.io/client-go/tools/leaderelection/resourcelock"
 )
 
 import (
@@ -23,6 +28,7 @@ import (
 func watchCommand() *cobra.Command {
 	var ingressClass string
 	var domainName string
+	var leaseName string
 
 	watchCmd := &cobra.Command{
 		Use:   "watch",
@@ -53,9 +59,55 @@ func watchCommand() *cobra.Command {
 				return err
 			}
 
-			var stop chan <- struct{}
+			var stop chan<- struct{}
 			informerFactory := informer(dm, clientset)
-			stop = runLoop(informerFactory)
+
+			if leaseName != "" {
+				// Validate fields present in env
+				podNamespace := os.Getenv("POD_NAMESPACE")
+				if podNamespace == "" {
+					return errors.New("must provide POD_NAMESPACE to use leases")
+				}
+
+				podName := os.Getenv("POD_NAME")
+				if podName == "" {
+					return errors.New("must provide POD_NAME to use leases")
+				}
+
+				leConfig := leaderelection.LeaderElectionConfig{
+					Lock: &resourcelock.LeaseLock{
+						LeaseMeta: metav1.ObjectMeta{
+							Name:      leaseName,
+							Namespace: podNamespace,
+						},
+						Client: clientset.CoordinationV1(),
+						LockConfig: resourcelock.ResourceLockConfig{
+							Identity: podName,
+						},
+					},
+					ReleaseOnCancel: true,
+					LeaseDuration:   30 * time.Second,
+					RenewDeadline:   15 * time.Second,
+					RetryPeriod:     5 * time.Second,
+					Callbacks: leaderelection.LeaderCallbacks{
+						OnStartedLeading: func(ctx context.Context) {
+							stop = runLoop(informerFactory)
+						},
+						OnStoppedLeading: func() {
+							stop <- struct{}{}
+						},
+					},
+				}
+
+				le, err := leaderelection.NewLeaderElector(leConfig)
+				if err != nil {
+					return err
+				}
+
+				le.Run(context.Background())
+			} else {
+				stop = runLoop(informerFactory)
+			}
 
 			sig := make(chan os.Signal, 1)
 			signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
@@ -69,6 +121,7 @@ func watchCommand() *cobra.Command {
 
 	watchCmd.Flags().StringVarP(&ingressClass, "ingress-class", "i", "", "ingress class to use for public DNS records")
 	watchCmd.Flags().StringVarP(&domainName, "domain", "d", "", "domain name for API calls")
+	watchCmd.Flags().StringVarP(&leaseName, "with-lease", "l", "", "use Kubernetes leases to control leader election")
 
 	return watchCmd
 }
@@ -105,7 +158,7 @@ func informer(dm *nsdns.DnsManager, clientset *kubernetes.Clientset) informers.S
 	return informerFactory
 }
 
-func runLoop(informerFactory informers.SharedInformerFactory) chan <- struct{} {
+func runLoop(informerFactory informers.SharedInformerFactory) chan<- struct{} {
 	stop := make(chan struct{})
 	informerFactory.Start(stop)
 	informerFactory.WaitForCacheSync(stop)
